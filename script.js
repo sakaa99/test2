2706afb9c604b90749365f0eee2438f1c50f6cbc
sakaa99 <satriaak99@gmail.com>
2025-06-10 13:33:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let umur = 22;
+if (umur <= 13) {
+    console.log("anak-anak");
+}
+else if (umur >= 17 && umur <= 20) {
+    console.log("remaja");
+}
+else if (umur>=20 && umur <=40 ){
+    console.log("dewasa");
+}
+else { console.log("lansia"); }
+
+let skor = 70
+if (skor<=50){
+    console.log("game over");
+}
+else{console.log("lanjut");}
